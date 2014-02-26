084603f786494f250cdc8df36bc7efb1d6dedfb3
Ed Watt <edwatt92@gmail.com>
2014-02-26 11:13:41 -0500

    Updates urls

--- linguini/urls.py
+++ linguini/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'linguini.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^translations/', include('translations.urls', namespace = "translations")),
     url(r'^admin/', include(admin.site.urls)),
 )
